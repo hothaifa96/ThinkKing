1974a10850592c4a24429a60ed1fbf9382965a20
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-11-29 03:12:08 +0200

    topics and kid login

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,22 @@
-from flask import (
-    Flask,
-    request
-)
+from flask import Flask
 # from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api, Resource
-
+from flask_restful import Api
 from api.resources import *
 
-# Application initialization
+
 app = Flask('Thinking')
 api = Api(app, prefix='/api')  # API Initialization
 
 api.add_resource(Hello, '/hello')
-api.add_resource(Parent, '/add/parent')
+api.add_resource(ParentRegister, '/add/parent')
 api.add_resource(ParentLogin, '/login/parent')
+api.add_resource(KidRegister, '/add/kid')
+api.add_resource(School, '/schools')
+api.add_resource(Class, '/classes')
+api.add_resource(SubClass, '/subclasses')
+api.add_resource(SubClass, '/subclasses')
+api.add_resource(Topics, '/topics')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/api/resources.py
+++ app/api/resources.py
@@ -32,7 +32,7 @@ class Hello(Resource):
         pass
 
 
-class Parent(Resource):
+class ParentRegister(Resource):
 
     def post(self):
         """
@@ -93,5 +93,209 @@ class ParentLogin(Resource):
         for k, v in parent_credentials.items():
             print(f"{k}: {v}")
 
-        return {'jwt':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'}
+        return {
+            'jwt': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'}
+
+
+class KidRegister(Resource):
+
+    def post(self):
+        """
+          Handle HTTP POST requests for creating a new kid entry.
+
+          Request: {"parent_id": "string", "kid_name": "string", "genre": "string"}
+
+          Response:
+          - 200 OK: Returns kid details with a generated "kid_id" and "success" flag.
+          - 400 Bad Request: If required keys are missing, returns an error message.
+
+          Response (Success): {"parent_id": "123", "kid_name": "Alice", "genre": "Adventure", "kid_id": "123", "success": true}
+          Response (Error): {"Error": "missing data"}
+        """
+        kid_credentials = request.json
+        required_keys = ['parent_id', 'kid_name', 'genre']
+
+        if not all(key in kid_credentials for key in required_keys):
+            return {'Error': 'missing data'}, 400
+
+        for k, v in kid_credentials.items():
+            print(f"{k}: {v}")
+
+        return {**kid_credentials, "kid_id": "123", "success": True}
+
+
+class School(Resource):
+    def get(self):
+        """
+           Handle HTTP GET requests to retrieve a list of schools.
+
+           Endpoint: /schools
+           Method: GET
+
+           Response:
+           - 200 OK: Returns a list of dictionaries, each representing a school with an ID and name.
+           """
+        schools = [
+            {'1': 'school1'},
+            {'2': 'school2'},
+            {'3': 'school3'}]
+        return schools
+
+    def post(self):
+        """
+            Handle HTTP POST requests for associating a kid with a school.
+
+            Endpoint: /schools
+            Method: POST
+
+            Request Format:
+            {
+                "kid_id": "string",     # ID of the kid to be associated with a school
+                "school_id": "string"   # ID of the school to associate the kid with
+            }
+
+            Response:
+            - 200 OK: Returns a success message.
+            - 400 Bad Request: If required keys are missing in the request, returns an error message.
+
+            """
+        kid_school = request.json
+        required_keys = ['kid_id', 'school_id']
+        if not all(key in kid_school for key in required_keys):
+            return {'Error': 'missing data'}, 400
+        print(kid_school)
+        return {'message': 'success'}
+
+
+class Class(Resource):
+    def get(self):
+        """
+           Handle HTTP GET requests to retrieve a list of Classes.
+
+           Endpoint: /class
+           Method: GET
+
+           Response:
+           - 200 OK: Returns a list of dictionaries, each representing a class with an ID and name.
+           """
+        try:
+            school_id = request.json['school_id']
+        except:
+            return {'message': 'school id is missing '}, 400
+
+        classes = [
+            {'1': 'class1'},
+            {'2': 'class2'},
+            {'3': 'class3'}]
+        return classes
+
+    def post(self):
+        """
+            Handle HTTP POST requests for associating a kid with a class.
+
+            Endpoint: /class
+            Method: POST
+
+            Request Format:
+            {
+                "kid_id": "string",    # ID of the kid to be associated with a class
+                "class_id": "string"   # ID of the class to associate the kid with
+            }
+
+            Response:
+            - 200 OK: Returns a success message.
+            - 400 Bad Request: If required keys are missing in the request, returns an error message.
+        """
+        kid_class = request.json
+        required_keys = ['kid_id', 'class_id']
+        if not all(key in kid_class for key in required_keys):
+            return {'Error': 'missing data'}, 400
+        print(kid_class)
+        return {'message': 'success'}
+
+
+class SubClass(Resource):
+    def get(self):
+        """
+           Handle HTTP GET requests to retrieve a list of Sub Classes.
+
+           Endpoint: /subclass
+           Method: GET
+
+           Response:
+           - 200 OK: Returns a list of dictionaries, each representing a subclass with an ID and name.
+           """
+        try:
+            class_id = request.json['class_id']
+        except:
+            return {'message': 'class id is missing '}, 400
+
+        subclass = [
+            {'1': 'sub_class1'},
+            {'2': 'sub_class2'},
+            {'3': 'sub_class3'}]
+        return subclass
+
+    def post(self):
+        """
+            Handle HTTP POST requests for associating a kid with a class.
+
+            Endpoint: /subclass
+            Method: POST
+
+            Request Format:
+            {
+                "kid_id": "string",    # ID of the kid to be associated with a class
+                "sub_class_id": "string"   # ID of the class to associate the kid with
+            }
+
+            Response:
+            - 200 OK: Returns a success message.
+            - 400 Bad Request: If required keys are missing in the request, returns an error message.
+        """
+        kid_sub_class = request.json
+        required_keys = ['kid_id', 'sub_class_id']
+        if not all(key in kid_sub_class for key in required_keys):
+            return {'Error': 'missing data'}, 400
+        print(kid_sub_class)
+        return {'message': 'success'}
+class Topics(Resource):
+    def get(self):
+        """
+          Handle HTTP GET requests to retrieve a list of topics.
+
+          Endpoint: /topics
+          Method: GET
+
+          Response:
+          - 200 OK: Returns a list of dictionaries, each representing a topic with an ID and name.
+        """
+        topics = [
+            {'1': 'math'},
+            {'2': 'english'}]
+        return topics
+
+    def post(self):
+        """
+            Handle HTTP POST requests for associating topics with a kid.
+
+            Endpoint: /topics
+            Method: POST
+
+            Request Format:
+            {
+                "kid_id": "string",    # ID of the kid to be associated with topics
+                "topics": ["string"]   # List of topic names to associate with the kid
+            }
+
+            Response:
+            - 200 OK: Returns a success message.
+            - 400 Bad Request: If required keys are missing in the request, returns an error message.
+        """
+        kid_topics = request.json
+        required_keys = ['kid_id', 'topics']
+        if not all(key in kid_topics for key in required_keys) :
+            return {'Error': 'missing data'}, 400
+        print(kid_topics)
+        return {'message': 'success'}
 
