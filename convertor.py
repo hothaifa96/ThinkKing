a00f56e2722eed8b7f5eabf04a23d1a64adf1ab2
hothaifa zoubi <hothaifazoubi@gmail.com>
2024-02-22 00:09:07 +0200

    message

--- convertor.py
+++ convertor.py
@@ -2,25 +2,14 @@ import pandas as pd
 import math
 
 files = [
-    '../s3/ארתמטיקה כתה ג.xlsx',
-    '../s3/ארתמטיקה כתה ד.xlsx',
-    '../s3/ידע עולם כתה ו 500 שאלות.xlsx',
-    '../s3/ידע עולם כתה ג 500 שאלות.xlsx',
-    '../s3/ידע עולם כתה ד 500 שאלות.xlsx',
-    '../s3/ידע עולם כתה ה 500 שאלות.xlsx',
-    '../s3/ידע עולם כתה ג 100 שאלות.xlsx',
-    '../s3/ארתמטיקה כתה ד 500 שאלות.xlsx',
-    '../s3/ידע עולם כתה ד 315 שאלות.xlsx',
-    '../s3/ידע עולם כתה ג 315 שאלות.xlsx',
-    '../s3/ידע עולם כתה ג.xlsx',
-    '../s3/c_knowlage.xlsx'
+    '../s3/updated_trivia_questions.xlsx',
 ]
 
 for url in files:
     df = pd.read_excel(url)
 
     # Create SQL file for INSERT commands
-    with open('/Users/hothaifa/Desktop/ThinkKing/hist/sql.sql', 'a+') as insert_data_file:
+    with open('/Users/hothaifa/Desktop/ThinkKing/hist/eng.sql', 'a+') as insert_data_file:
         insert_data_file.write(f'-- new file here {url[url.rfind("/") + 1:]}--\n')
 
         # Insert data into topics table (avoid duplicates)
@@ -34,7 +23,7 @@ for url in files:
                 topic_id = int(str(row.iloc[1])[0])  # Assuming the second column (index 1) is qouestion_id
                 print(f'topic_id = {topic_id}')
                 if topic_id not in inserted_topics:
-                    topic_name = 'math' if topic_id == 1 else 'common knowlage'
+                    topic_name = 'math' if topic_id == 1 else 'common knowlage' if  topic_id == 3 else 'english'
                     insert_data_file.write(
                         f"INSERT INTO topics (topic_id, topic_name) VALUES ({topic_id}, '{topic_name}') ON CONFLICT (topic_id) DO NOTHING;\n")
                     inserted_topics.add(topic_id)
@@ -51,9 +40,9 @@ for url in files:
             c_grade_id = int(question_id[2])  # Set c_grade_id to the last digit of qouestion_id
             level = int(question_id[-1])
 
-            q_top = 2 if topic_id == 3 else 3
+            q_top = 2 if topic_id == 3 or topic_id == 4 else 3
             question_text = str(row.iloc[q_top]).replace("'", "`")
-            if topic_id == 3:
+            if topic_id == 3 or topic_id:
                 explanation = ''
                 interesting_fact = str(row.iloc[7]).replace("'", "`")
             else:
@@ -66,7 +55,7 @@ INSERT INTO questions (question_id, language_id, topic_id, c_grade_id, level, qu
 VALUES ('{question_id}', {language_id}, {topic_id}, {c_grade_id}, {level}, '{question_text}', '{explanation}', '{interesting_fact}') ON CONFLICT (question_id) DO UPDATE SET language_id = {language_id} ,topic_id= {topic_id},c_grade_id ={c_grade_id},level={level},question_text='{question_text}',explanation='{explanation}',interesting_fact='{interesting_fact}';
 """)
 
-            q_id = 3 if topic_id == 3 else 4
+            q_id = 3 if topic_id == 3 or topic_id ==4  else 4
             correct_answer = str(row.iloc[q_id]).replace("'",
                                                          "`").strip()  # Assuming the fourth column (index 3) is right_answer
             insert_data_file.write(f"""
@@ -74,7 +63,7 @@ INSERT INTO answer_options (question_id, correct_answer, answer_text)
 VALUES ('{question_id}', TRUE, '{correct_answer}') ;
 """)
 
-            q_id = 4 if topic_id == 3 else 5
+            q_id = 4 if topic_id == 3 or topic_id ==4 else 5
             wrong_answers = [str(row.iloc[q_id]).replace("'", "`").strip(),str(row.iloc[q_id + 1]).replace("'", "`").strip(), str(row.iloc[q_id + 2]).replace("'","`").strip()]
             if 'nan' in wrong_answers:
                 wrong_answers.remove('nan')
@@ -87,11 +76,11 @@ INSERT INTO answer_options (question_id, correct_answer, answer_text)
 VALUES ('{question_id}', FALSE, '{answer}');
 """)
     
-    insert_data_file.write("""DELETE FROM answer_options
-WHERE answer_option_id::text NOT IN (
-    SELECT MIN(answer_option_id)::text
-    FROM answer_options
-    GROUP BY question_id, answer_text
-);""")
+#     insert_data_file.write("""DELETE FROM answer_options
+# WHERE answer_option_id::text NOT IN (
+#     SELECT MIN(answer_option_id)::text
+#     FROM answer_options
+#     GROUP BY question_id, answer_text
+# );""")
 
     print("INSERT commands SQL file generated successfully.")
